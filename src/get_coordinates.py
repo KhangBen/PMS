3b21b934aadd96fbdc00cb5d954397fe4824da85
Khang Hoang <khangben367@gmail.com>
2026-03-21 17:21:12 -0700

    got better parking spot boundary points

--- src/get_coordinates.py
+++ src/get_coordinates.py
@@ -2,7 +2,14 @@ import os
 import cv2
 # import sys
 
+from detector import Detector
+from parking_spots import parking_spots, is_occupied
+from drawing import draw_parking_spots
+
 points = []
+
+# constructors
+detector = Detector()
 
 # File Path Setup : picking video file
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -29,15 +36,42 @@ video = cv2.VideoCapture(video_path)
 
 # main loop : runs until quit
 while True:
+  # car coordinates
+  cars = []
   # read frame
   ret, frame = video.read()
+  h, w, _ = frame.shape
+  # bottom half only for detection
+  roi = frame[h//2:, :]
   # if not break out of loop
   if not ret:
     print("Error: video file not found")
     break
 
   for point in points:
-    cv2.circle(frame, point, 5, (0, 0, 255), -1)
+    cv2.circle(frame, point, 5, (255, 255, 255), -1)
+
+  offset_y = frame.shape[0] // 2 # fream height offset
+      
+  results = detector.detect(roi)
+
+  # getting car coordinates
+  for box in results[0].boxes:
+    x1, y1, x2, y2 = map(int, box.xyxy[0])
+    y1 += offset_y
+    y2 += offset_y
+    cars.append((x1, y1, x2, y2))
+
+  # get the center point of car
+  # cx = (x1 + x2) // 2
+  # cy = (y1 + y2) // 2
+  # getting 75% on y based on camera perspective
+  # cy = int(y1 + 0.75 * (y2 - y1)) 
+
+  # getting center of each car deteciton
+  # cv2.circle(frame, (cx, cy), 5, (255, 0, 0), 2) 
+
+  frame = draw_parking_spots(frame, parking_spots, cars, is_occupied)
 
   # Show Frame : opens window
   cv2.imshow("Frame", frame)
